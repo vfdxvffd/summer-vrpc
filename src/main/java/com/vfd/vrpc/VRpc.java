726d423037ca510ea3560d182e1f2605c02e3bc8
vfdxvffd <2201986113@qq.com>
2021-05-13 00:45:14 +0800

    [增加] 在summer框架中的扩展，将远程对象的代理对象的设置，服务提供的开启，以及操作句柄的注入都在此完成

---
+++ src/main/java/com/vfd/vrpc/VRpc.java
@@ -0,0 +1,226 @@
+package com.vfd.vrpc;
+
+import com.vfd.summer.applicationContext.impl.SummerAnnotationConfigApplicationContext;
+import com.vfd.summer.extension.Extension;
+import com.vfd.summer.ioc.bean.BeanDefinition;
+import com.vfd.vrpc.annotation.Reference;
+import com.vfd.vrpc.config.Config;
+import com.vfd.vrpc.handler.RpcRequestMessageHandler;
+import com.vfd.vrpc.handler.RpcResponseMessageHandler;
+import com.vfd.vrpc.message.RpcRequestMessage;
+import com.vfd.vrpc.protocol.MessageCodec;
+import com.vfd.vrpc.protocol.ProtocolFrameDecoder;
+import com.vfd.vrpc.protocol.SequenceIdGenerator;
+import com.vfd.vrpc.protocol.serializer.Serializer;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.util.concurrent.DefaultPromise;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Proxy;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * @PackageName: com.vfd.v-rpc
+ * @ClassName: VRpc
+ * @Description:
+ * @author: vfdxvffd
+ * @date: 2021/5/12 下午2:20
+ */
+public class VRpc implements Extension {
+
+    public final Map<Integer, Channel> channelMap = new ConcurrentHashMap<>();
+    private Channel channel = null;
+    RpcResponseMessageHandler RPC_HANDLER = new RpcResponseMessageHandler();
+
+    boolean keepAlive = Config.keepAlive();
+
+    //记录关键位置的日志
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Override
+    public void doOperation0 (SummerAnnotationConfigApplicationContext context) {
+
+    }
+
+    @Override
+    public void doOperation1 (SummerAnnotationConfigApplicationContext context) {
+
+    }
+
+
+    // 把控制、操作、修改rpc配置的句柄装配到ioc容器中
+    @Override
+    public void doOperation2 (SummerAnnotationConfigApplicationContext context) {
+        context.getIocByName().put("vRpcHandler", new VRpcHandler(this));
+        final HashSet<String> set = new HashSet<>();
+        set.add("vRpcHandler");
+        context.getBeanTypeAndName().put(VRpcHandler.class, set);
+    }
+
+    // 对二级缓存中标注了@Reference注解的域进行注入
+    @Override
+    public void doOperation3 (SummerAnnotationConfigApplicationContext context) throws Exception {
+        for (Map.Entry<String, Object> objectEntry : context.getEarlyRealObjects().entrySet()) {
+            referenceObject(objectEntry.getValue());
+        }
+        logger.info("远程调用的代理对象设置完成");
+    }
+
+    private void referenceObject (Object object) throws Exception {
+        final Class<?> clazz = object.getClass();
+        for (Field field : clazz.getDeclaredFields()) {
+            final Reference reference = field.getAnnotation(Reference.class);
+            if (reference != null) {
+                field.setAccessible(true);
+                if (field.get(object) != null) {
+                    continue;
+                }
+                String host = "".equals(reference.host())? Config.getDestHost():reference.host();
+                int port = reference.port() == -1? Config.getDestPort():reference.port();
+                Serializer serializer = reference.serializer()==Serializer.class? Config.getDestSerializer():reference.serializer().newInstance();
+                final Object remoteObj = getRemoteObj(host, port, serializer, reference.beanName(), field.getType());
+                field.set(object, remoteObj);
+            }
+        }
+    }
+
+    @SuppressWarnings("all")
+    private <T> T getRemoteObj (String destHost, int destPort, Serializer serializer,
+                                 String beanName, Class<T> beanType) {
+        final Object o = Proxy.newProxyInstance(beanType.getClassLoader(), new Class[] {beanType},
+                ((proxy, method, args) -> {
+                    final int sequenceId = SequenceIdGenerator.nextId();
+                    RpcRequestMessage msg = new RpcRequestMessage(
+                            sequenceId,
+                            beanName,
+                            beanType,
+                            method.getName(),
+                            method.getReturnType(),
+                            method.getParameterTypes(),
+                            args
+                    );
+                    final Channel ch = getChannel(destHost, destPort, serializer);
+                    ch.writeAndFlush(msg);
+                    DefaultPromise<Object> promise = new DefaultPromise<>(ch.eventLoop());
+                    RPC_HANDLER.getPROMISES().put(sequenceId, promise);
+                    promise.await();
+                    if (promise.isSuccess()) {
+                        final Object now = promise.getNow();
+                        if (!keepAlive)
+                            closeConnect();
+                        return now;
+                    } else {
+                        final Throwable cause = promise.cause();
+                        closeConnect();
+                        throw new RuntimeException(cause);
+                    }
+                }));
+        return (T) o;
+    }
+
+    private Channel getChannel (String destHost, int destPort, Serializer serializer) {
+        if (channel == null)
+            initChannel(destHost, destPort, serializer);
+        return channel;
+    }
+
+    private void initChannel (String destHost, int destPort, Serializer serializer) {
+        EventLoopGroup group = new NioEventLoopGroup();
+        Bootstrap bootstrap = new Bootstrap();
+        MessageCodec MESSAGE_CODEC = new MessageCodec(serializer);
+        bootstrap.group(group)
+                .channel(NioSocketChannel.class)
+                .handler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    protected void initChannel(SocketChannel socketChannel) {
+                        final ChannelPipeline pipeline = socketChannel.pipeline();
+                        pipeline.addLast(new ProtocolFrameDecoder())
+                                //.addLast(new LoggingHandler(LogLevel.DEBUG))
+                                .addLast(MESSAGE_CODEC)
+                                .addLast(RPC_HANDLER);
+                    }
+                });
+        try {
+            channel = bootstrap.connect(destHost, destPort).sync().channel();
+            final ChannelFuture channelFuture = channel.closeFuture();
+            channelFuture.addListener(future -> group.shutdownGracefully());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // 开启服务端监听端口，对外提供服务
+    @Override
+    public void doOperation4 (SummerAnnotationConfigApplicationContext context) throws Exception {
+        if (!Config.providerServer()) {
+            return;
+        }
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        MessageCodec MESSAGE_CODEC = new MessageCodec(Config.getServerSerializer());
+        RpcRequestMessageHandler RPC_HANDLER = new RpcRequestMessageHandler(context);
+        int port = Config.getServerPort();
+        try {
+            ServerBootstrap serverBootStrap = new ServerBootstrap();
+            serverBootStrap.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) {
+                            final ChannelPipeline pipeline = socketChannel.pipeline();
+                            pipeline.addLast(new ProtocolFrameDecoder())
+                                    //.addLast(new LoggingHandler(LogLevel.DEBUG))
+                                    .addLast(MESSAGE_CODEC)
+                                    .addLast(RPC_HANDLER);
+                        }
+                    });
+            Channel channel = serverBootStrap.bind(port).sync().channel();
+            channelMap.put(port, channel);
+            final ChannelFuture channelFuture = channel.closeFuture();
+            channelFuture.addListener(future -> {
+                bossGroup.shutdownGracefully();
+                workerGroup.shutdownGracefully();
+            });
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        logger.info("远程服务提供开启，在端口: " + port + "监听");
+    }
+
+    public void closeConnect () {
+        if (channel != null)    channel.close();
+        channel = null;
+        RPC_HANDLER = new RpcResponseMessageHandler();
+    }
+
+    @Override
+    public void doOperation5 (SummerAnnotationConfigApplicationContext context, BeanDefinition beanDefinition) {
+
+    }
+
+    @Override
+    public void doOperation6 (SummerAnnotationConfigApplicationContext context, Object o) {
+
+    }
+
+    // 对此对象进行检查，如果其中的域包含了@Reference注解，则对其进行注入
+    @Override
+    public void doOperation7 (SummerAnnotationConfigApplicationContext context, Object o) throws Exception {
+        referenceObject(o);
+    }
+
+    @Override
+    public void doOperation8 (SummerAnnotationConfigApplicationContext context, Object o) {
+
+    }
+}
