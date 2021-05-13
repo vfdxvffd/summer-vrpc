2725a987b3c31227ba4a3645b090765b4965386e
vfdxvffd <2201986113@qq.com>
2021-05-13 20:10:10 +0800

    [增加] 增加多种操作连接的handler,提供多种控制连接的方法

--- src/main/java/com/vfd/vrpc/VRpcHandler.java
+++ src/main/java/com/vfd/vrpc/VRpcHandler.java
@@ -1,5 +1,8 @@
 package com.vfd.vrpc;
 
+import com.vfd.vrpc.config.Config;
+import com.vfd.vrpc.protocol.Destination;
+import com.vfd.vrpc.protocol.serializer.Serializer;
 import io.netty.channel.Channel;
 
 /**
@@ -11,24 +14,47 @@ import io.netty.channel.Channel;
  */
 public class VRpcHandler {
 
-    VRpc vRpc = new VRpc();
+    VRpcAdapter vRpcAdapter;
 
-    public VRpcHandler(VRpc vRpc) {
-        this.vRpc = vRpc;
+    public VRpcHandler(VRpcAdapter vRpcAdapter) {
+        this.vRpcAdapter = vRpcAdapter;
     }
 
     public void keepAlive () {
-        vRpc.keepAlive = true;
+        vRpcAdapter.keepAlive = true;
     }
 
     public void keepNonAlive () {
-        vRpc.keepAlive = false;
+        vRpcAdapter.keepAlive = false;
     }
 
-    public void closeProvideServer (int port) {
-        final Channel channel = vRpc.channelMap.getOrDefault(port, null);
+    public void closeProvider (int port) {
+        final Channel channel = vRpcAdapter.serverChannelMap.getOrDefault(port, null);
         if (channel != null)    channel.close();
+        vRpcAdapter.serverChannelMap.remove(port);
     }
 
+    public void closeAllProvider () {
+        for (Integer port : vRpcAdapter.serverChannelMap.keySet()) {
+            closeProvider(port);
+        }
+    }
 
+    public void startProvideServer (int port, Serializer serializer) throws Exception {
+        vRpcAdapter.provide0(port, serializer);
+    }
+
+    public void startProvideServer (int port) throws Exception {
+        startProvideServer(port, Config.getServerSerializer());
+    }
+
+    public void disableReference (String host, int port, Serializer serializer) {
+        vRpcAdapter.closeConnect(new Destination(host, port, serializer));
+    }
+
+    public void disableAllReference () {
+        for (Destination destination : vRpcAdapter.clientChannelMap.keySet()) {
+            vRpcAdapter.closeConnect(destination);
+        }
+    }
 }
